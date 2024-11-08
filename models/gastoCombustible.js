05a0490928e65f8bb8a3b0ceb230989b3b82ad8e
rosafraile67 <mfraile@birt.eus>
2024-11-08 14:10:13 +0100

    First commit

---
+++ models/gastoCombustible.js
@@ -0,0 +1,17 @@
+export class GastoCombustible {
+    constructor(vehicleType, date, kilometers, precioViaje) {
+        this.vehicleType = vehicleType;
+        this.date = date;
+        this.kilometers = kilometers;
+        this.precioViaje = precioViaje;
+    }
+
+    convertToJSON() {
+        return JSON.stringify({
+            vehicleType: this.vehicleType,
+            date: this.date,
+            kilometers: this.kilometers,
+            precioViaje: this.precioViaje
+        });
+    }
+}
